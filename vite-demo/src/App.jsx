48995d698c9e57e89f583157db6084e99b4ba35d
Julien Kerekes <julien.kerekes@gmail.com>
2024-02-11 01:47:40 +0100

    Added fields for Query Input and Output and a button to send it.

--- vite-demo/src/App.jsx
+++ vite-demo/src/App.jsx
@@ -1,22 +1,26 @@
 import {useState} from 'react';
 import './App.css';
+import QueryForm from './components/QueryForm';
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
 
   return (
     <>
       <div id='header'>
         <h1>BuQL Demo</h1>
       </div>
-      <div className='card'>
+      <div id='demo'>
+        <QueryForm></QueryForm>
+      </div>
+      {/* <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      </div> */}
       <div id='footer'>
         <h2>By J, J, J & DC</h2>
       </div>
