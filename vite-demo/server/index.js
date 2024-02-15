a54e67d42e2371940bc01b097fbfae8addd46869
Julien Kerekes <julien.kerekes@gmail.com>
2024-02-15 19:10:14 +0100

    Added new queries for the demo Commented out a security line that was breaking the frontend Updated package.json
    Co-authored-by: jldiamond <140023753+jldiamond@users.noreply.github.com>

--- vite-demo/server/index.js
+++ vite-demo/server/index.js
@@ -5,39 +5,42 @@ const app = express();
 const port = 8080;
 
 // import graphql and schema
-import { graphqlHTTP } from 'express-graphql';
-import { schema } from './schema/schema';
+import {graphqlHTTP} from 'express-graphql';
+import {schema} from './schema/schema';
 
 //imports for validationRules (security)
-import depthLimit from 'graphql-depth-limit'
-import { createComplexityLimitRule } from 'graphql-validation-complexity';
+import depthLimit from 'graphql-depth-limit';
+import {createComplexityLimitRule} from 'graphql-validation-complexity';
 
 /* this will be an optional method on the BuQL object as a whole;
 user will pull it from the created BuQL object (const security = BuQL.security) 
 then, when passing it in as middleware, invoke it with custom options if they need ('/endpoint', security(custom options), (req, res) => ... )*/
-function RulesCreator (givenLimit = 10, costLimit = 1000, customRules) {
+function RulesCreator(givenLimit = 10, costLimit = 1000, customRules) {
   //define the default list of rules; source: https://github.com/4Catalyzer/graphql-validation-complexity
   const defaultRules = {
     scalarCost: 1,
     objectCost: 0,
     listFactor: 10,
-    introspectionListFactor: 2
-  }
+    introspectionListFactor: 2,
+  };
   //check if user has passed in custom options
-  if (customRules){
+  if (customRules) {
     //verify that custom options is an object (but not an array)
-    if (typeof customRules !== 'object' || Array.isArray(customRules)){throw new Error('customRules should be an object')}
+    if (typeof customRules !== 'object' || Array.isArray(customRules)) {
+      throw new Error('customRules should be an object');
+    }
     //combine defaultRules with the custom rules
-    Object.assign(defaultRules, customRules)
+    Object.assign(defaultRules, customRules);
   }
   //return the costlimit and default rules
-  return [depthLimit(givenLimit), costLimit, defaultRules]
+  return [depthLimit(givenLimit), costLimit, defaultRules];
 }
 /* there's two ways this can go:
       1. set this up as a ternary, checking if the user has called it or something (what i have right now)
       2. make sure the user pulls and invokes it, whether or not they use the options */
 
-const rules = /* something ? */ RulesCreator() /* : RulesCreator(things, user, passes, in) */
+const rules =
+  /* something ? */ RulesCreator(); /* : RulesCreator(things, user, passes, in) */
 
 // configure cors, json parsing, url encoding
 const corsOptions = {
@@ -64,7 +67,7 @@ app.use(
 
 // Forward request to this middleware if not in cache
 app.use('/buqlQuery', buqlController.addCache, (req, res) => {
-  return res.status(202).json({ message: 'successfully buqled' });
+  return res.status(202).json({message: 'successfully buqled'});
 });
 
 // Clear cache route
@@ -73,15 +76,19 @@ app.use('/clearCache', buqlController.clearCache, (req, res) => {
 });
 
 // Standalone graphql route
-app.use('/graphql', securityController.checkChars, graphqlHTTP({ 
-  schema, 
-  graphiql: true,
-  //validation rules for security
-  validationRules: [...rules]
-}));
+app.use(
+  '/graphql',
+  securityController.checkChars,
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+    //validation rules for security
+    //validationRules: [...rules]
+  })
+);
 
 app.get('/', (req, res) => {
-  return res.status(200).json({ BuQL: 'up!' });
+  return res.status(200).json({BuQL: 'up!'});
 });
 
 app.use('*', (req, res) => res.status(404).send('Page not found'));
@@ -90,7 +97,7 @@ app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught an unknown middlware error',
     status: 500,
-    message: { err: 'An error occurred' },
+    message: {err: 'An error occurred'},
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj);
--- vite-demo/src/components/Queries.js
+++ vite-demo/src/components/Queries.js
@@ -1,13 +1,6 @@
 // define queries that will be available to choose from in the demo. might put in a different file?
 const queries = [
-  /*{
-    label: 'Grab all users with the initial J',
-    query: '',
-  },
-  {
-    label: 'Grab all posts created after Feb 9th 2024',
-    query: '',
-  }, */
+  /* Working set of queries before schema change
   {
     label: 'Query: Grab all users with the userId 1',
     query: 'query { user(userId: 1) { name } }',
@@ -28,6 +21,34 @@ const queries = [
     label: 'Mutation: Create User with name and password',
     query:
       'mutation { createUser (username: "Filip", password: "aGx75C6hz2!_") { id username password } }',
+  }, */
+  {
+    label: 'Grab all users',
+    query: 'query { getAllUsers { id username email birthdate registeredAt } }',
+  },
+  {
+    label: 'Grab the user with the userId 65cdf731aeda2e240baec9fc',
+    query:
+      'query { getUserById(id: "65cdf731aeda2e240baec9fc") { id username email birthdate registeredAt } }',
+  },
+  {
+    label: 'Add a random new user',
+    query:
+      'mutation { addRandomUsers(num: 1) { id username email birthdate registeredAt } }',
+  },
+  {
+    label: 'Delete the user with the userId 65cdf731aeda2e240baec9fc',
+    query:
+      'mutation { deleteById(id: "65cdf731aeda2e240baec9fc") { id username email birthdate registeredAt } }',
+  },
+  {
+    label: 'Bad (not Michael Jackson bad) Query',
+    query: 'query { getAccount { id username email } }',
+  },
+  {
+    label: 'Query with illegal character',
+    query:
+      'query { getUserById(id: "65cdf731aeda2e240baec9fc" OR 1=1) { id username email } }',
   },
 ];
 
