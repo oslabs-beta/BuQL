1edb7a46e71d052238aa8d8ee5cad81e9b1a85fa
Joseph-McGarry <jrmcgarry88@gmail.com>
2024-02-27 20:15:45 -0500

    minor styling changes, root margin fixed

--- vite-demo/src/components/AboutUs.jsx
+++ vite-demo/src/components/AboutUs.jsx
@@ -3,19 +3,17 @@ import './AboutUs.css'
 function AboutUs(){
     return(
     <div id='aboutUs'>
-            <h1>Meet the Team</h1>
-            <h2></h2>
             <div className="team-container">
                 <container className="team-member">
                     <h2 >Dylan Compton</h2>
-                    <p>Demolitions Expert. </p>
+                    <p>Wild Card. </p>
                     <p>Crazy impersonations. </p>
                     <p>Good hair.</p>   
                 </container>
                 <div className="team-member">
                     <h2>Jake Diamond</h2>
                     <p>Mastermind.</p>
-                    <p>Eagles fan...probably</p>
+                    <p>Eagles fan, probably.</p>
                     <p>Diamonds are forever.</p>
                 </div>
                 <div className="team-member">
@@ -27,7 +25,7 @@ function AboutUs(){
                 <div className="team-member">
                     <h2>Joseph McGarry</h2> 
                     <p>Scavenger Mentality.</p>  
-                    <p>Chicken Wing Aficionado</p>  
+                    <p>Chicken Wing Aficionado.</p>  
                     <p>Girthy.</p>    
                 </div>
             </div>
