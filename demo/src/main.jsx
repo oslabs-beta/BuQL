8946d55d07577338ba4b98065fb70d6cc784d2ba
Joseph-McGarry <jrmcgarry88@gmail.com>
2024-02-29 17:48:43 -0500

    hopefully fixed error in main.jsx

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './assets/github-mark.svg'
-import './assets/linkedin2.svg'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
