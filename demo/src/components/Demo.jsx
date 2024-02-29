1c7f43eb3a35e4834e00409128f401df0cb6bc3b
Joseph-McGarry <jrmcgarry88@gmail.com>
2024-02-29 13:27:40 -0500

    vercel website rendering background only. adding demo component and styles

--- demo/src/components/Demo.jsx
+++ demo/src/components/Demo.jsx
@@ -1,1 +1,19 @@
+import './Demo.css';
+
+function Demo(){
+    return (
+        <div>
+            <div id='demo'>
+                <h1>Demo Gif goes here!</h1>
+            </div>
+        </div>
+    );
+};
+
+
+
+
+
+
+
 export default Demo;
