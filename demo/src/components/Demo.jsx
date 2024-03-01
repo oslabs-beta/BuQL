bc54f22bf7813a8d6e29c33c247faa6cc8870d4e
Julien Kerekes <julien.kerekes@gmail.com>
2024-03-01 05:37:56 +0100

    Added demo videos to demo page

--- demo/src/components/Demo.jsx
+++ demo/src/components/Demo.jsx
@@ -2,9 +2,15 @@ import './Demo.css';
 
 function Demo(){
     return (
-        <div id='demo'>
-            <h1>Demo Gif goes here!</h1>
-        </div>
+      <div id='demo'>
+        <h1>Demo</h1>
+        <h2>Field-level caching for your GraphQL queries:</h2>
+        <img className='demovid' src='https://i.imgur.com/WJ2t1we.gif' alt='Video showing field level caching'></img>
+        <h2>Handling invalid and illegal queries:</h2>
+        <img className='demovid' src='https://i.imgur.com/85r30Ol.gif' alt='Video showing security features'></img>
+        <h2>BuQL supports mutations, too:</h2>
+        <img className='demovid' src='https://i.imgur.com/4hvl022.gif' alt='Video showing successful mutation queries'></img>
+      </div>
     );
 }
 
