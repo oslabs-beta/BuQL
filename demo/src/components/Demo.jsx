31b918f6beadd530c979e0e8d9e2a49e3341c2c1
julien-kerekes <julien.kerekes@stud.hs-bochum.de>
2024-03-13 01:12:43 +0100

    Refined the text on the landing page

--- demo/src/components/Demo.jsx
+++ demo/src/components/Demo.jsx
@@ -6,8 +6,10 @@ function Demo(){
         <h1>Demo</h1>
         <h2>Field-level caching for your GraphQL queries:</h2>
         <img className='demovid' src='https://i.imgur.com/WJ2t1we.gif' alt='Video showing field level caching'></img>
+        <br/>
         <h2>Handling invalid and illegal queries:</h2>
         <img className='demovid' src='https://i.imgur.com/85r30Ol.gif' alt='Video showing security features'></img>
+        <br/>
         <h2>BuQL supports mutations, too:</h2>
         <img className='demovid' src='https://i.imgur.com/4hvl022.gif' alt='Video showing successful mutation queries'></img>
       </div>
--- demo/src/components/Home.jsx
+++ demo/src/components/Home.jsx
@@ -4,11 +4,25 @@ function Home(){
     return (
         <div>
             <div id='home'>
-                <h1>Explained</h1>
-                <h2>Light-weight NPM package for caching and querying in GraphQL</h2>
-                <p>BuQL is a lightweight NPM packing for server-side GraphQL caching in the Bun runtime environment.
-                    Boasting ZERO dependencies, our open source product, is designed retrieve queries with lighting speed
-                    from your Redis cache. 
+                <h1>BuQL:</h1>
+                <h2>Light-weight NPM package for intercepting and caching in GraphQL</h2>
+                <p>BuQL is a tool designed to address the specific needs of developers working with GraphQL 
+                    in the Bun runtime environment. Recognizing the importance of efficient data management, 
+                    BuQL integrates with Redis to offer improved performance in data retrieval and caching. 
+                    This solution aims to streamline the handling of server-side GraphQL queries, 
+                    enhancing speed without compromising on reliability or security.
+                </p>
+                <p>The development and continuous improvement of BuQL are driven by community feedback and collaboration. 
+                    We value the insights and contributions from developers who use BuQL in their projects, 
+                    as these experiences are crucial for evolving the tool’s capabilities. 
+                    Looking ahead, BuQL’s development includes plans to introduce features 
+                    like client-side caching and expanded security options, 
+                    with the goal of supporting a broader range of GraphQL applications.
+                </p>
+                <p>We encourage developers interested in optimizing their GraphQL implementations to explore BuQL’s capabilities. 
+                    Our GitHub repository provides detailed information and documentation to help you get started, 
+                    and our LinkedIn community offers a platform for discussion, feedback, and sharing best practices. 
+                    BuQL represents our commitment to enhancing GraphQL performance, driven by real-world applications and developer insights.                    
                 </p>
             </div>
         </div>
