4c26d9f384e00b76825bf23ec0955864bc84761d
Julien Kerekes <julien.kerekes@gmail.com>
2024-03-28 15:07:34 +0100

    Fixed small typos

--- demo/src/components/Home.jsx
+++ demo/src/components/Home.jsx
@@ -14,12 +14,12 @@ function Home(){
                 </p>
                 <p>The development and continuous improvement of BuQL are driven by community feedback and collaboration. 
                     We value the insights and contributions from developers who use BuQL in their projects, 
-                    as these experiences are crucial for evolving the tool’s capabilities. 
-                    Looking ahead, BuQL’s development includes plans to introduce features 
+                    as these experiences are crucial for evolving the tool's capabilities. 
+                    Looking ahead, BuQL's development includes plans to introduce features 
                     like client-side caching and expanded security options, 
                     with the goal of supporting a broader range of GraphQL applications.
                 </p>
-                <p>We encourage developers interested in optimizing their GraphQL implementations to explore BuQL’s capabilities. 
+                <p>We encourage developers interested in optimizing their GraphQL implementations to explore BuQL's capabilities. 
                     Our GitHub repository provides detailed information and documentation to help you get started, 
                     and our LinkedIn community offers a platform for discussion, feedback, and sharing best practices. 
                     BuQL represents our commitment to enhancing GraphQL performance, driven by real-world applications and developer insights.                    
